22019b4abc001e429f97183e71328a637b8b0bbb
David Pfahler <pfahler.david@gmail.com>
2019-05-26 10:34:47 +0200

    added search field

---
+++ DSASearchField.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: '90%',
+  }
+});
+
+function DSASearchField(props) {
+  const {label,
+    placeholder,
+    classes,
+    helperText,
+    value,
+    onChange} = props;
+  return <TextField
+        id="standard-search"
+        label={label}
+        placeholder={placeholder}
+        helperText={helperText}
+        type="search"
+        className={classes.textField}
+        margin="normal"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+}
+
+DSASearchField.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(DSASearchField);
